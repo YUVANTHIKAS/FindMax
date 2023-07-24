d2f45264ba99157454096eb8551e8819502df096
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-24 15:16:56 +0530

    [yuvanthika] Add:implemented

--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FindMax;
+using System;
 
 namespace FinMax
 {
@@ -10,6 +11,12 @@ namespace FinMax
             operation.FindMaxInt(1, 2, 3);
             operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
             operation.FindMaxString("a", "b", "c");
+
+            GenericOperation genericOperation = new GenericOperation();
+            Console.WriteLine("\n\nGeneric:\n");
+            genericOperation.FindMax<int>(1, 2, 3);
+            genericOperation.FindMax<float>(1.1f, 1.9f, 1.4f);
+            genericOperation.FindMax<string>("a", "b", "c");
         }
     }
 }
