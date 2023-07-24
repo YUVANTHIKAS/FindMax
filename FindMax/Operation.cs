972c2f88c4cb54e0eaa95bca5e391582a89cf58e
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-24 16:48:27 +0530

    [yuvanthika]:implemented

--- FindMax/Operation.cs
+++ FindMax/Operation.cs
@@ -4,39 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinMax
+namespace FindMax
 {
     public class Operation
     {
-        public void FindMaxInt(int a, int b, int c)
+
+        public void FindMaxInteger(int a, int b, int c)
         {
             if (a >= b && a >= c)
-                Console.WriteLine("a is max");
+            {
+                Console.WriteLine("int a is max");
+            }
             if (b >= a && b >= c)
-                Console.WriteLine("b is max");
+            {
+                Console.WriteLine("int b is max");
+            }
             if (c >= a && c >= b)
-                Console.WriteLine(" c is max");
+            {
+                Console.WriteLine("int c is max");
+            }
         }
         public void FindMaxFloat(float a, float b, float c)
         {
             if (a >= b && a >= c)
-                Console.WriteLine("a is max");
+            {
+                Console.WriteLine("float a is max");
+            }
             if (b >= a && b >= c)
-                Console.WriteLine("b is max");
+            {
+                Console.WriteLine("float b is max");
+            }
             if (c >= a && c >= b)
-                Console.WriteLine(" c is max");
+            {
+                Console.WriteLine("float c is max");
+            }
         }
         public void FindMaxString(string a, string b, string c)
         {
             if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
-                Console.WriteLine("a is max");
+                Console.WriteLine("string a is max");
             if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
-                Console.WriteLine("b is max");
+                Console.WriteLine("string b is max");
             if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
-                Console.WriteLine(" c is max");
+                Console.WriteLine("string c is max");
         }
-
-
     }
 }
-
--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -1,22 +1,75 @@
-﻿using FindMax;
-using System;
-
-namespace FinMax
+﻿using System;
+namespace FindMax
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Operation operation = new Operation();
-            operation.FindMaxInt(1, 2, 3);
-            operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
-            operation.FindMaxString("a", "b", "c");
+            //Operation operation = new Operation();
+            //operation.FindMaxInteger(1, 2, 3);
+            //operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
+            //operation.FindMaxString("a", "b", "c");
+
+
+
 
             GenericOperation genericOperation = new GenericOperation();
             Console.WriteLine("\n\nGeneric:\n");
-            genericOperation.FindMax<int>(1, 2, 3);
-            genericOperation.FindMax<float>(1.1f, 1.9f, 1.4f);
-            genericOperation.FindMax<string>("a", "b", "c");
+
+
+
+            bool flag = true;
+
+            Console.WriteLine("Enter the size of  array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Enter the datatype:\n 1.Integer\n 2.Float\n 3.String\n 4.Exit");
+
+
+
+
+            while (flag)
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+
+
+
+                    case 1:
+                        int[] arr = new int[size];
+                        for (int i = 0; i < size; i++)
+                        {
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        genericOperation.FindMax<int>(arr);
+                        break;
+                    case 2:
+                        float[] Farr = new float[size];
+                        for (int i = 0; i < size; i++)
+                        {
+                            Farr[i] = Convert.ToSingle(Console.ReadLine());
+                        }
+                        genericOperation.FindMax<float>(Farr);
+                        break;
+                    case 3:
+                        string[] Sarr = new string[size];
+                        for (int i = 0; i < size; i++)
+                        {
+                            Sarr[i] = Console.ReadLine();
+                        }
+                        genericOperation.FindMax<string>(Sarr);
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
+
