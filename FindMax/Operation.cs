27a3c7200bd1d31d20be70f41d3a614c27c726ac
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-24 14:34:54 +0530

    [yuvanthika] Add:created

---
+++ FindMax/Operation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinMax
+{
+    public class Operation
+    {
+        public void FindMaxInt(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+                Console.WriteLine("a is max");
+            if (b >= a && b >= c)
+                Console.WriteLine("b is max");
+            if (c >= a && c >= b)
+                Console.WriteLine(" c is max");
+        }
+        public void FindMaxFloat(float a, float b, float c)
+        {
+            if (a >= b && a >= c)
+                Console.WriteLine("a is max");
+            if (b >= a && b >= c)
+                Console.WriteLine("b is max");
+            if (c >= a && c >= b)
+                Console.WriteLine(" c is max");
+        }
+        public void FindMaxString(string a, string b, string c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+                Console.WriteLine("a is max");
+            if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+                Console.WriteLine("b is max");
+            if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
+                Console.WriteLine(" c is max");
+        }
+
+
+    }
+}
+
---
+++ FindMax/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.FindMaxInt(1, 2, 3);
+            operation.FindMaxFloat(1.2f, 1.6f, 1.1f);
+            operation.FindMaxString("a", "b", "c");
+        }
+    }
+}
